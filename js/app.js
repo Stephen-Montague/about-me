115d53c801a9a45f07632cfebfc61c876caff3de
Brian Sward <bsward@gmail.com>
2022-07-14 17:41:12 -0700

    final changes

--- js/app.js
+++ js/app.js
@@ -9,10 +9,50 @@ let userName = prompt('Hello, stranger. These popups are here to demo some basic
 if (userName !== null) { // If user clicks 'Cancel' skip all popups.
   alert(`Welcome to the site ${userName}! 
   Please guess on the following facts about me.`);
-  lifeExperienceGame();
+
+  let question0 = 'Yes or no... I have 7 lifetimes of experience. (Enter Y/N)';
+  let question1 = 'Yes or no... I have 7 daughters. (Enter Y/N)';
+  let question2 = 'Yes or no... I have experience in game development. (Enter Y/N)';
+  let question3 = 'Yes or no... I have a BA in International Studies: East Asia. (Enter Y/N)';
+  let question4 = 'Yes or no... I have Japanese language skills. (Enter Y/N)';
+
+  let gameQuestions = [question0, question1, question2, question3, question4];
+  let gameAnswers = ['n', 'n', 'y', 'y', 'y'];
+
+  lifeExperienceGame(gameQuestions,gameAnswers);
+  luckyNumberGuessingGame();
+  favoriteThingsGame();
 
   alert(`Wow, ${userName}, you got ${gameScore} right so far.`);
 
+} // End initial Cancel option, to load page immediately.
+
+function lifeExperienceGame(gameQuestions,gameAnswers) {
+
+  let rightAnswerMsg = 'Nice. You guessed right.';
+  let wrongAnswerMsg = 'That\'s not quite right, but let\'s continue.';
+  let invalidAnswerMsg = 'I\'m sorry, but that\'s not a valid answer.';
+
+  for (let i = 0; i < 5; i++) {
+    rawGameInput = prompt(gameQuestions[i]).toLowerCase();
+    if (rawGameInput === 'y' || rawGameInput === 'n' ||
+      rawGameInput === 'yes' || rawGameInput === 'no') {
+      validGameInput = rawGameInput.charAt(0);
+      if (validGameInput) {
+        if (validGameInput === gameAnswers[i]) {
+          alert(rightAnswerMsg);
+          ++gameScore;
+        } else {
+          alert(wrongAnswerMsg);
+        }
+      } // End if(validGameInput)
+    } else {
+      alert(invalidAnswerMsg);
+    }
+  } // End for loop handling the life experience game.
+}
+
+function luckyNumberGuessingGame() {
   // Lucky Number Guessing Game
   let totalGuessCount = 4;
   let finalGuessIndex = totalGuessCount - 1;
@@ -49,7 +89,9 @@ if (userName !== null) { // If user clicks 'Cancel' skip all popups.
       }
     }
   } // End for loop handling the lucky number guess counter.
+}
 
+function favoriteThingsGame() {
   // Favorite Things Guessing Game
   let fruitPromptMsg0 = 'Let\'s play one last game. Your score is now: ';
   let fruitPromptMsg1 = '\nGuess my 5 favorite tropical fruit. 1 point per right guess.\nNumber of guesses: ';
@@ -58,8 +100,8 @@ if (userName !== null) { // If user clicks 'Cancel' skip all popups.
   let totalFruit = fruitNames.length;
   guessedFruitNames.length = totalFruit;
   let fruitGameScore = 0;
-  totalGuessCount = 7;
-  finalGuessIndex = totalGuessCount - 1;
+  let totalGuessCount = 7;
+  let finalGuessIndex = totalGuessCount - 1;
   validGameInput = null;
   for (let usedGuessCount = 0; usedGuessCount < totalGuessCount; usedGuessCount++) {
     if (usedGuessCount === 0) {
@@ -93,43 +135,10 @@ if (userName !== null) { // If user clicks 'Cancel' skip all popups.
       }
       if (usedGuessCount === finalGuessIndex) {
         alert(`That was the last guess. My favorite tropical fruits: 
-        ${fruitNames[0]}, ${fruitNames[1]}, ${fruitNames[2]}, ${fruitNames[3]}, ${fruitNames[4]}.
-        You guessed ${fruitGameScore} of ${totalFruit} fruit.
-        All Games Final Score: ${gameScore}`);
+      ${fruitNames[0]}, ${fruitNames[1]}, ${fruitNames[2]}, ${fruitNames[3]}, ${fruitNames[4]}.
+      You guessed ${fruitGameScore} of ${totalFruit} fruit.
+      All Games Final Score: ${gameScore}`);
       }
     }
   } // End fruit guess-counter for loop
-} // End initial Cancel option, to load page immediately.
-
-function lifeExperienceGame() {
-  let question0 = 'Yes or no... I have 7 lifetimes of experience. (Enter Y/N)';
-  let question1 = 'Yes or no... I have 7 daughters. (Enter Y/N)';
-  let question2 = 'Yes or no... I have experience in game development. (Enter Y/N)';
-  let question3 = 'Yes or no... I have a BA in International Studies: East Asia. (Enter Y/N)';
-  let question4 = 'Yes or no... I have Japanese language skills. (Enter Y/N)';
-
-  let rightAnswerMsg = 'Nice. You guessed right.';
-  let wrongAnswerMsg = 'That\'s not quite right, but let\'s continue.';
-  let invalidAnswerMsg = 'I\'m sorry, but that\'s not a valid answer.';
-
-  let gameQuestions = [question0, question1, question2, question3, question4];
-  let gameAnswers = ['n', 'n', 'y', 'y', 'y'];
-
-  for (let i = 0; i < 5; i++) {
-    rawGameInput = prompt(gameQuestions[i]).toLowerCase();
-    if (rawGameInput === 'y' || rawGameInput === 'n' ||
-      rawGameInput === 'yes' || rawGameInput === 'no') {
-      validGameInput = rawGameInput.charAt(0);
-      if (validGameInput) {
-        if (validGameInput === gameAnswers[i]) {
-          alert(rightAnswerMsg);
-          ++gameScore;
-        } else {
-          alert(wrongAnswerMsg);
-        }
-      } // End if(validGameInput)
-    } else {
-      alert(invalidAnswerMsg);
-    }
-  } // End for loop handling the life experience game.
 }
